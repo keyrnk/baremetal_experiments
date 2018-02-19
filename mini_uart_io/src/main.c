94fab73bef2acf75990a832a2b217404850eab14
Karina Dorozhkina <Dorozhkina@avp.ru>
2018-02-19 15:35:27 +0300

    'simplify_impl'

--- mini_uart_io/src/main.c
+++ mini_uart_io/src/main.c
@@ -1,250 +1,114 @@
-#include <stddef.h>
-#include <stdint.h>
-/* 
-// Memory-Mapped I/O output
-static inline void mmio_write(uint32_t reg, uint32_t data)
+#define PERIPHERAL_BASE_ADDRESS 0x3F000000
+
+#define AUX_ENABLE_REG 0x3F215004
+
+#define AUX_MU_IO_REG 0x3F215040
+#define AUX_MU_IIR_REG 0x3F215044
+#define AUX_MU_IER_REG 0x3F215048
+#define AUX_MU_LCR_REG 0x3F21504C
+#define AUX_MU_MCR_REG 0x3F215050
+#define AUX_MU_LSR_REG 0x3F215054
+#define AUX_MU_MSR_REG 0x3F215058
+#define AUX_MU_SCRATCH_REG 0x3F21505C
+#define AUX_MU_CNTL_REG 0x3F215060
+#define AUX_MU_STAT_REG 0x3F215064
+#define AUX_MU_BAUD_REG 0x3F215068
+
+#define GPFSEL1 0x3F200004
+#define GPPUD 0x3F200094
+#define GPPUDCLK0 0x3F200098
+
+void WriteToRegister(unsigned int reg, const unsigned int val)
 {
-	*(volatile uint32_t*)reg = data;
+	*(volatile unsigned int*)reg = val;
 }
- 
-// Memory-Mapped I/O input
-static inline uint32_t mmio_read(uint32_t reg)
+
+void ReadFromRegister(unsigned int reg, unsigned int* val)
 {
-	return *(volatile uint32_t*)reg;
+	*val = *(volatile unsigned int*)reg;
 }
- 
-// Loop <delay> times in a way that the compiler won't optimize away
-static inline void delay(int32_t count)
+
+static void SetUpGPIO(void)
 {
-	asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
-		 : "=r"(count): [count]"0"(count) : "cc");
+	//alternate pin 14 and pin 15 to miniuart tx/rx mode
+	unsigned int reg;
+	ReadFromRegister(GPFSEL1, &reg);
+
+	//clean pin 14 bits (12-14)
+	reg &= ~(7 << 12);
+
+	//clean pin 15 bits (15-17)
+	reg &= ~(7 << 15);
+
+	//set pin 14/15 alt5 function for tx1/rx1
+	reg |= (2 << 12);
+	reg |= (2 << 15);
+
+	WriteToRegister(GPFSEL1, reg);
+
+	WriteToRegister(GPPUD, 0);
+
+	//according to broadcom documentation we need to wait 150 cycles
+	const unsigned int CyclesNum = 150; 
+	unsigned int i;
+	for(i = 0; i < CyclesNum; i++) 
+		dummy(i);
+
+    	WriteToRegister(GPPUDCLK0,(1<<14)|(1<<15));
+    	for(i = 0; i < CyclesNum; i++) 
+		dummy(i);
+
+    	WriteToRegister(GPPUDCLK0, 0);
 }
- 
-enum
-{   
-    BASE = 0x3F000000,
- 
-    // The offsets for reach register.
- 
-    // Controls actuation of pull up/down to ALL GPIO pins.
-    GPPUD = (GPIO_BASE + 0x94),
- 
-    // Controls actuation of pull up/down for specific GPIO pin.
-    GPPUDCLK0 = (GPIO_BASE + 0x98),
- 
-    // The base address for UART.
-    UART0_BASE = 0x3F201000, // for raspi2 & 3, 0x20201000 for raspi1
- 
-    // The offsets for reach register for the UART.
-    UART0_DR     = (UART0_BASE + 0x00),
-    UART0_RSRECR = (UART0_BASE + 0x04),
-    UART0_FR     = (UART0_BASE + 0x18),
-    UART0_ILPR   = (UART0_BASE + 0x20),
-    UART0_IBRD   = (UART0_BASE + 0x24),
-    UART0_FBRD   = (UART0_BASE + 0x28),
-    UART0_LCRH   = (UART0_BASE + 0x2C),
-    UART0_CR     = (UART0_BASE + 0x30),
-    UART0_IFLS   = (UART0_BASE + 0x34),
-    UART0_IMSC   = (UART0_BASE + 0x38),
-    UART0_RIS    = (UART0_BASE + 0x3C),
-    UART0_MIS    = (UART0_BASE + 0x40),
-    UART0_ICR    = (UART0_BASE + 0x44),
-    UART0_DMACR  = (UART0_BASE + 0x48),
-    UART0_ITCR   = (UART0_BASE + 0x80),
-    UART0_ITIP   = (UART0_BASE + 0x84),
-    UART0_ITOP   = (UART0_BASE + 0x88),
-    UART0_TDR    = (UART0_BASE + 0x8C),
-};
- 
-void uart_init()
+
+void InitMiniUart()
 {
-	// Disable UART0.
-	mmio_write(UART0_CR, 0x00000000);
-	// Setup the GPIO pin 14 && 15.
- 
-	// Disable pull up/down for all GPIO pins & delay for 150 cycles.
-	mmio_write(GPPUD, 0x00000000);
-	delay(150);
- 
-	// Disable pull up/down for pin 14,15 & delay for 150 cycles.
-	mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
-	delay(150);
- 
-	// Write 0 to GPPUDCLK0 to make it take effect.
-	mmio_write(GPPUDCLK0, 0x00000000);
- 
-	// Clear pending interrupts.
-	mmio_write(UART0_ICR, 0x7FF);
- 
-	// Set integer & fractional part of baud rate.
-	// Divider = UART_CLOCK/(16 * Baud)
-	// Fraction part register = (Fractional part * 64) + 0.5
-	// UART_CLOCK = 3000000; Baud = 115200.
- 
-	// Divider = 3000000 / (16 * 115200) = 1.627 = ~1.
-	mmio_write(UART0_IBRD, 1);
-	// Fractional part register = (.627 * 64) + 0.5 = 40.6 = ~40.
-	mmio_write(UART0_FBRD, 40);
- 
-	// Enable FIFO & 8 bit data transmissio (1 stop bit, no parity).
-	mmio_write(UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
- 
-	// Mask all interrupts.
-	mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
-	                       (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
- 
-	// Enable UART0, receive & transfer part of UART.
-	mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
+	const unsigned int MiniUartEnable = 0x1;
+	WriteToRegister(AUX_ENABLE_REG, MiniUartEnable);
+	WriteToRegister(AUX_MU_IER_REG, 0);
+	
+	const unsigned int FifoClearEnable = 0xC6;
+	WriteToRegister(AUX_MU_IIR_REG, FifoClearEnable);
+	
+	const unsigned int eightBitUartMode = 0x01;
+	WriteToRegister(AUX_MU_LCR_REG, eightBitUartMode);
+	WriteToRegister(AUX_MU_MCR_REG, 0);
+	WriteToRegister(AUX_MU_CNTL_REG, 0);
+	WriteToRegister(AUX_MU_BAUD_REG, 270);
+
+	SetUpGPIO();
+
+	const unsigned int ReceiverTransmitterEnable = 0x03;
+	WriteToRegister(AUX_MU_CNTL_REG, ReceiverTransmitterEnable);
 }
- 
-void uart_putc(unsigned char c)
+
+void GetChar(char* ch)
 {
-	// Wait for UART to become ready to transmit.
-	while ( mmio_read(UART0_FR) & (1 << 5) ) { }
-	mmio_write(UART0_DR, c);
-}
- 
-unsigned char uart_getc()
-{
-    // Wait for UART to have received something.
-    while ( mmio_read(UART0_FR) & (1 << 4) ) { }
-    return mmio_read(UART0_DR);
-}
- 
-void uart_puts(const char* str)
-{
-	for (size_t i = 0; str[i] != '\0'; i ++)
-		uart_putc((unsigned char)str[i]);
-}
- 
-#if defined(__cplusplus)
-extern "C" /* Use C linkage for kernel_main. */
-/*#endif
-void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
-{
-	// Declare as unused
-	(void) r0;
-	(void) r1;
-	(void) atags;
- 
-	uart_init();
-	uart_puts("Hello, kernel World!\r\n");
- 
+	const unsigned int DataReadyBit = 0x01;
 	while (1)
-		uart_putc(uart_getc());
-}*/
+	{
+		unsigned int lsrRegVal;
+		ReadFromRegister(AUX_MU_LSR_REG, &lsrRegVal);
+		if (lsrRegVal & DataReadyBit)
+			break;
+	}
 
-//Uncomment one of these
-//#include "BCM2835.h" /* Original B,A,A+,B+ */
-#define PBASE 0x3F000000
-
-extern void PUT32 ( unsigned int, unsigned int );
-extern unsigned int GET32 ( unsigned int );
-extern void dummy ( unsigned int );
-
-#define GPFSEL1         (PBASE+0x00200004)
-#define GPSET0          (PBASE+0x0020001C)
-#define GPCLR0          (PBASE+0x00200028)
-#define GPPUD           (PBASE+0x00200094)
-#define GPPUDCLK0       (PBASE+0x00200098)
-
-#define AUX_ENABLES     (PBASE+0x00215004)
-#define AUX_MU_IO_REG   (PBASE+0x00215040)
-#define AUX_MU_IER_REG  (PBASE+0x00215044)
-#define AUX_MU_IIR_REG  (PBASE+0x00215048)
-#define AUX_MU_LCR_REG  (PBASE+0x0021504C)
-#define AUX_MU_MCR_REG  (PBASE+0x00215050)
-#define AUX_MU_LSR_REG  (PBASE+0x00215054)
-#define AUX_MU_MSR_REG  (PBASE+0x00215058)
-#define AUX_MU_SCRATCH  (PBASE+0x0021505C)
-#define AUX_MU_CNTL_REG (PBASE+0x00215060)
-#define AUX_MU_STAT_REG (PBASE+0x00215064)
-#define AUX_MU_BAUD_REG (PBASE+0x00215068)
-
-//GPIO14  TXD0 and TXD1
-//GPIO15  RXD0 and RXD1
-//alt function 5 for uart1
-//alt function 0 for uart0
-
-//((250,000,000/115200)/8)-1 = 270
-//------------------------------------------------------------------------
-void uart_putc ( unsigned int c )
-{
-    while(1)
-    {
-        if(GET32(AUX_MU_LSR_REG)&0x20) break;
-    }
-    PUT32(AUX_MU_IO_REG,c);
+	ReadFromRegister(AUX_MU_IO_REG, (unsigned int*)ch);
 }
-//------------------------------------------------------------------------
-void hexstrings ( unsigned int d )
-{
-    //unsigned int ra;
-    unsigned int rb;
-    unsigned int rc;
 
-    rb=32;
-    while(1)
-    {
-        rb-=4;
-        rc=(d>>rb)&0xF;
-        if(rc>9) rc+=0x37; else rc+=0x30;
-        uart_putc(rc);
-        if(rb==0) break;
-    }
-    uart_putc(0x20);
+void PutChar(const char ch)
+{
+	WriteToRegister(AUX_MU_IO_REG, (const unsigned int)ch);
 }
-//------------------------------------------------------------------------
-void hexstring ( unsigned int d )
+
+void notmain(void)
 {
-    hexstrings(d);
-    uart_putc(0x0D);
-    uart_putc(0x0A);
-}
-//------------------------------------------------------------------------
-int notmain ( void )
-{
-    unsigned int ra;
+	InitMiniUart();
 
-    PUT32(AUX_ENABLES,1);
-    PUT32(AUX_MU_IER_REG,0);
-    PUT32(AUX_MU_CNTL_REG,0);
-    PUT32(AUX_MU_LCR_REG,3);
-    PUT32(AUX_MU_MCR_REG,0);
-    PUT32(AUX_MU_IER_REG,0);
-    PUT32(AUX_MU_IIR_REG,0xC6);
-    PUT32(AUX_MU_BAUD_REG,270);
-
-    ra=GET32(GPFSEL1);
-    ra&=~(7<<12); //gpio14
-    ra|=2<<12;    //alt5
-    ra&=~(7<<15); //gpio15
-    ra|=2<<15;    //alt5
-    PUT32(GPFSEL1,ra);
-
-    PUT32(GPPUD,0);
-    for(ra=0;ra<150;ra++) dummy(ra);
-    PUT32(GPPUDCLK0,(1<<14)|(1<<15));
-    for(ra=0;ra<150;ra++) dummy(ra);
-    PUT32(GPPUDCLK0,0);
-
-    PUT32(AUX_MU_CNTL_REG,3);
-
-    hexstring(0x12345678);
-
-    while(1)
-    {
-        while(1)
-        {
-            if(GET32(AUX_MU_LSR_REG)&0x01) break;
-        }
-        ra=GET32(AUX_MU_IO_REG);
-        //while(1)
-        //{
-            //if(GET32(AUX_MU_LSR_REG)&0x20) break;
-        //}
-        PUT32(AUX_MU_IO_REG,ra);
-    }
-
-    return(0);
+	while(1)
+	{
+		char ch;
+		GetChar(&ch);
+		PutChar(ch);
+	}
 }
