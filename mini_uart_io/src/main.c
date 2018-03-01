dc66975deb0e1c36beefba17bf1a881d3ae94ea5
Karina Dorozhkina <karina.dorozhkina@kaspersky.com>
2018-03-01 19:25:08 +0300

    'add_put_str'

--- mini_uart_io/src/main.c
+++ mini_uart_io/src/main.c
@@ -1,76 +1,73 @@
-#define PERIPHERAL_BASE_ADDRESS 0x3F000000
+#define MMIO_BASE       0x3F000000
 
-#define AUX_ENABLE_REG 0x3F215004
+#define AUX_ENABLE_REG      ((volatile unsigned int*)(MMIO_BASE+0x00215004))
+#define AUX_MU_IO_REG       ((volatile unsigned int*)(MMIO_BASE+0x00215040))
+#define AUX_MU_IER_REG      ((volatile unsigned int*)(MMIO_BASE+0x00215044))
+#define AUX_MU_IIR_REG     ((volatile unsigned int*)(MMIO_BASE+0x00215048))
+#define AUX_MU_LCR_REG      ((volatile unsigned int*)(MMIO_BASE+0x0021504C))
+#define AUX_MU_MCR_REG      ((volatile unsigned int*)(MMIO_BASE+0x00215050))
+#define AUX_MU_LSR_REG      ((volatile unsigned int*)(MMIO_BASE+0x00215054))
+#define AUX_MU_MSR_REG      ((volatile unsigned int*)(MMIO_BASE+0x00215058))
+#define AUX_MU_SCRATCH_REG  ((volatile unsigned int*)(MMIO_BASE+0x0021505C))
+#define AUX_MU_CNTL_REG     ((volatile unsigned int*)(MMIO_BASE+0x00215060))
+#define AUX_MU_STAT_REG     ((volatile unsigned int*)(MMIO_BASE+0x00215064))
+#define AUX_MU_BAUD_REG     ((volatile unsigned int*)(MMIO_BASE+0x00215068))
+#define GPFSEL1_REG        ((volatile unsigned int*)(MMIO_BASE+0x00200004))
+#define GPPUD_REG           ((volatile unsigned int*)(MMIO_BASE+0x00200094))
+#define GPPUDCLK0_REG       ((volatile unsigned int*)(MMIO_BASE+0x00200098))
 
-#define AUX_MU_IO_REG 0x3F215040
-#define AUX_MU_IIR_REG 0x3F215044
-#define AUX_MU_IER_REG 0x3F215048
-#define AUX_MU_LCR_REG 0x3F21504C
-#define AUX_MU_MCR_REG 0x3F215050
-#define AUX_MU_LSR_REG 0x3F215054
-#define AUX_MU_MSR_REG 0x3F215058
-#define AUX_MU_SCRATCH_REG 0x3F21505C
-#define AUX_MU_CNTL_REG 0x3F215060
-#define AUX_MU_STAT_REG 0x3F215064
-#define AUX_MU_BAUD_REG 0x3F215068
-
-#define GPFSEL1 0x3F200004
-#define GPPUD 0x3F200094
-#define GPPUDCLK0 0x3F200098
-
-void WriteToRegister(unsigned int reg, const unsigned int val)
+void WriteToRegister(volatile unsigned int* reg, const unsigned int val)
 {
-	*(volatile unsigned int*)reg = val;
+	*reg = val;
 }
 
-void ReadFromRegister(unsigned int reg, unsigned int* val)
+void ReadFromRegister(volatile unsigned int* reg, unsigned int* val)
 {
-	*val = *(volatile unsigned int*)reg;
+	*val = *reg;
 }
 
 static void SetUpGPIO(void)
 {
 	//alternate pin 14 and pin 15 to miniuart tx/rx mode
 	unsigned int reg;
-	ReadFromRegister(GPFSEL1, &reg);
+	ReadFromRegister(GPFSEL1_REG, &reg);
 
 	//clean pin 14 bits (12-14)
-	reg &= ~(7 << 12);
-
-	//clean pin 15 bits (15-17)
-	reg &= ~(7 << 15);
+	reg &= ~((7 << 12) | (7 << 15));
 
 	//set pin 14/15 alt5 function for tx1/rx1
 	reg |= (2 << 12);
 	reg |= (2 << 15);
 
-	WriteToRegister(GPFSEL1, reg);
+	WriteToRegister(GPFSEL1_REG, reg);
 
-	WriteToRegister(GPPUD, 0);
+	WriteToRegister(GPPUD_REG, 0);
 
 	//according to broadcom documentation we need to wait 150 cycles
 	const unsigned int CyclesNum = 150; 
 	unsigned int i;
 	for(i = 0; i < CyclesNum; i++) 
-		dummy(i);
+		asm volatile("nop");
 
-    	WriteToRegister(GPPUDCLK0,(1<<14)|(1<<15));
+    	WriteToRegister(GPPUDCLK0_REG, ((1<<14)|(1<<15)));
     	for(i = 0; i < CyclesNum; i++) 
-		dummy(i);
+		asm volatile("nop");
 
-    	WriteToRegister(GPPUDCLK0, 0);
+    	WriteToRegister(GPPUDCLK0_REG, 0);
 }
 
 void InitMiniUart()
 {
-	const unsigned int MiniUartEnable = 0x1;
-	WriteToRegister(AUX_ENABLE_REG, MiniUartEnable);
+	unsigned int auxEnableReg;
+	ReadFromRegister(AUX_ENABLE_REG, &auxEnableReg);
+	auxEnableReg |= 1;
+	WriteToRegister(AUX_ENABLE_REG, auxEnableReg);
 	WriteToRegister(AUX_MU_IER_REG, 0);
 	
 	const unsigned int FifoClearEnable = 0xC6;
 	WriteToRegister(AUX_MU_IIR_REG, FifoClearEnable);
 	
-	const unsigned int eightBitUartMode = 0x01;
+	const unsigned int eightBitUartMode = 3;
 	WriteToRegister(AUX_MU_LCR_REG, eightBitUartMode);
 	WriteToRegister(AUX_MU_MCR_REG, 0);
 	WriteToRegister(AUX_MU_CNTL_REG, 0);
@@ -82,9 +79,19 @@ void InitMiniUart()
 	WriteToRegister(AUX_MU_CNTL_REG, ReceiverTransmitterEnable);
 }
 
-void GetChar(char* ch)
+char GetChar() 
 {
 	const unsigned int DataReadyBit = 0x01;
+    	char r;
+    	do{asm volatile("nop");}while(!(*AUX_MU_LSR_REG & DataReadyBit));
+
+    	r=(char)(*AUX_MU_IO_REG);
+   	return r=='\r'?'\n':r;
+}
+
+void PutChar(const char ch)
+{
+	const unsigned int DataReadyBit = 0x20;
 	while (1)
 	{
 		unsigned int lsrRegVal;
@@ -93,22 +100,28 @@ void GetChar(char* ch)
 			break;
 	}
 
-	ReadFromRegister(AUX_MU_IO_REG, (unsigned int*)ch);
-}
-
-void PutChar(const char ch)
-{
 	WriteToRegister(AUX_MU_IO_REG, (const unsigned int)ch);
 }
 
-void notmain(void)
+void PutStr(const char* s)
+{
+	while(*s != '\0')
+	{
+		if (*s == '\n')
+			PutChar('\r');
+
+		PutChar(*s);
+		s++;
+	}
+}
+
+void main(void)
 {
 	InitMiniUart();
 
+	PutStr("Hello fucking world!\n");
 	while(1)
 	{
-		char ch;
-		GetChar(&ch);
-		PutChar(ch);
+		PutChar(GetChar());
 	}
 }
--- mini_uart_io/src/start.s
+++ mini_uart_io/src/start.s
@@ -3,8 +3,13 @@
 .global _start
 
 _start:
+    mrs x1, mpidr_el1
+    and x1, x1, #3
+    cbz x1, 2f
+1:  wfe
+    b 1b
     // set stack before our code
-    ldr     r0, =_start
-    mov     sp, r0
+2:  ldr     x1, =_start
+    mov     sp, x1
     bl      main
     b       .
