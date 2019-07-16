d3c9ea818f397f4dcfd0d22ce914308dfe241a69
dorozhkina <dorozhkina@Dorozhkina-KOS>
2019-07-16 14:00:59 +0300

    make_allocator_stateless

--- mini_uart_io_cpp/arena_allocator.h
+++ mini_uart_io_cpp/arena_allocator.h
@@ -20,11 +20,11 @@ struct ArenaAllocator
 	using size_type = std::size_t;
 	using difference_type = std::ptrdiff_t;
 
-	//static const pointer arena_start = &heap_start;
-	//static const size_type arena_size = &heap_end - &heap_start;
-	static const size_type block_size = 64;
+	static const size_type BlockSize = 64;
+	static const size_type MaxHeapSize = 1024;
 
-	struct Block {
+	struct Block 
+	{
 		Block(pointer newAddress, size_type newSize)
 			: address(newAddress)
 			, size(newSize)
@@ -40,36 +40,61 @@ struct ArenaAllocator
 		size_type size;
 	};
 	
-	struct Arena {
+	struct Arena {	
 		Arena()
 		{}
 
-		Arena(size_type initSize, pointer startAddress)
-			: size(initSize)
-			, curAddress(startAddress)
+		Arena(size_type heapSize, pointer startAddress)
+			: blocksNum(heapSize / BlockSize)
+			, startAddress(startAddress)
 			, index(0)
 			, freeList(nullptr)
 		{}
 
-		std::array<Block, 1024 / block_size> blocks;
-		
-		size_type size;
-		pointer curAddress;
+		std::array<Block, MaxHeapSize / BlockSize> blocks;
+	
+		size_type blocksNum;
+		pointer startAddress;
 		size_type index; 
 		Block* freeList;
 	};
 
-	Arena arena;
+	static Arena arena;
 
-	ArenaAllocator()
-		: arena((&heap_end - &heap_start), &heap_start)
+	ArenaAllocator() = default;
+
+	pointer allocate(size_type n, ArenaAllocator<T>::const_pointer hint = 0)
 	{
+		cout << "allocate\n";
+		if (n > BlockSize)
+			return nullptr;
+
+		pointer address;
+		if (arena.freeList != nullptr)
+		{
+			Block* oldHead = arena.freeList;
+			oldHead->next = nullptr;
+			address = oldHead->address;
+			arena.freeList = arena.freeList->next;
+		}
+		else
+		{
+			if (arena.index >= arena.blocksNum)
+			{
+				return nullptr;
+			}
+			arena.blocks[arena.index].address = arena.startAddress + arena.index * BlockSize;
+			arena.blocks[arena.index].size = BlockSize;
+			++arena.index;
+		}
+
+		return address;
 	}
 
 	void deallocate(pointer p, size_type n)
 	{
-		pointer startAddress = arena.blocks[0].address;
-		size_type blockIndex = (p - startAddress) / block_size;
+		cout << "deallocate\n";
+		size_type blockIndex = (p - arena.startAddress) / BlockSize;
 		if (arena.freeList == nullptr)
 		{
 			arena.freeList = &arena.blocks[blockIndex];
@@ -80,39 +105,11 @@ struct ArenaAllocator
 			arena.freeList = &arena.blocks[blockIndex];
 			arena.freeList->next = oldFreeList;
 		}
-
-		arena.size += block_size;
-	}
-
-	pointer allocate(size_type n, ArenaAllocator<T>::const_pointer hint = 0)
-	{
-		
-		if (n > arena.size)
-			return nullptr;
-
-		//the size of freelist block isnt the same as required
-		//assume all are equal
-		pointer address;
-		if (arena.freeList != nullptr)
-		{
-			Block* newHead = arena.freeList;
-			arena.freeList = arena.freeList->next;
-			arena.size -= newHead->size;
-			address = newHead->address;
-		}
-		else
-		{
-			arena.blocks[arena.index].address = arena.curAddress;
-			address = arena.curAddress;
-			arena.blocks[arena.index].size = block_size;
-			++arena.index;
-			arena.size -= block_size;
-			arena.curAddress += block_size;
-		}
-
-		cout << "allocate\n";
-		return address;
 	}
 
 };
+
+template <class T>
+typename ArenaAllocator<T>::Arena ArenaAllocator<T>::arena((&heap_end - &heap_start), &heap_start);
+
 #endif
